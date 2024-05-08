bea2099900b83dd8cac93ce228eb056c696f1172
Daniel Reardon <ddreardon@gmail.com>
2024-05-08 11:46:47 -0700

    add leafnode subclass, test

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,5 +1,5 @@
 class HTMLNode():
-    def __init__(self, tag=None, value=None, children=None, props=None):
+    def __init__(self, tag=None, value=None, children=None, props={}):
         self.tag = tag
         self.value = value
         self.children = children
@@ -16,3 +16,17 @@ class HTMLNode():
 
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+    
+class LeafNode(HTMLNode):
+    def __init__(self, value, tag=None, props=None):
+        if props is None:
+            props = {}
+        super().__init__(tag=tag, value=value, children=None, props=props)
+        if self.value == None:
+            raise ValueError
+
+    def to_html(self):
+        if self.tag == None:
+            return self.value
+        else:
+            return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,11 +1,17 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHtmlNode(unittest.TestCase):
     def test_props_to_html(self):
         node1 = HTMLNode(props={"key1": "value1", "key2": "value2"})
         self.assertEqual(node1.props_to_html(), ' key1="value1" key2="value2"')
+
+class TestLeafNode(unittest.TestCase):
+    def test_to_html(self):
+        node1 = LeafNode(tag = "t", value = "value", props = {"key1": "value1", "key2": "value2"})
+        self.assertEqual(node1.to_html(), '<t key1="value1" key2="value2">value</t>')
+
 
 if __name__ == "__main__":
     unittest.main()
