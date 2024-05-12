d997e1ecc720695a8da01d05b3428c662cbb09b4
Daniel Reardon <ddreardon@gmail.com>
2024-05-12 14:53:52 -0700

    Add function for converting text node to html node

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -47,4 +47,19 @@ class ParentNode(HTMLNode):
             string += node.to_html()
         return f"<{self.tag}{self.props_to_html()}>{string}</{self.tag}>"
             
-            
+def text_node_to_html_node(text_node):
+    if text_node.text_type == "text":
+        return LeafNode(value = text_node.text)
+    if text_node.text_type == "bold":
+        return LeafNode(value = text_node.text, tag = "b")
+    if text_node.text_type == "italic":
+        return LeafNode(value = text_node.text, tag="i")
+    if text_node.text_type == "code":
+        return LeafNode(value = text_node.text, tag = "code")
+    if text_node.text_type == "link":
+        return LeafNode(value = text_node.text, tag = "a", prop = {"href" : text_node.url})
+    if text_node.text_type == "image":
+        return LeafNode(value = "", tag = "img", props={"src": text_node.url, "alt": text_node.text})
+    raise Exception("Unsupported text type")
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
