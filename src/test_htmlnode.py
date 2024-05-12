cd3a2cacaf4bf2e3478b9998976d11c2cee6c9cb
Daniel Reardon <ddreardon@gmail.com>
2024-05-12 14:29:08 -0700

    Add test of parent to_html method

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 class TestHtmlNode(unittest.TestCase):
     def test_props_to_html(self):
@@ -11,6 +11,9 @@ class TestHtmlNode(unittest.TestCase):
         node1 = LeafNode(tag = "t", value = "value", props = {"key1": "value1", "key2": "value2"})
         self.assertEqual(node1.to_html(), '<t key1="value1" key2="value2">value</t>')
 
+    def test_to_html_parent(self):
+        node1 = ParentNode(tag = "a", children = [ParentNode(tag="b", children = [LeafNode(tag = "c", value = "value1")]), LeafNode(tag = "d", value="value2")], props = {"key3": "value3", "key4":"value4"})
+        self.assertEqual(node1.to_html(), '<a key3="value3" key4="value4"><b><c>value1</c></b><d>value2</d></a>')
 
 
 if __name__ == "__main__":
