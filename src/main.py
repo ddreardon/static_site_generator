8b853afe403bedcacbee73411aec319deddef33b
Daniel Reardon <ddreardon@gmail.com>
2024-05-15 15:15:15 -0700

    add public to gitignore, clarified main.py

--- src/main.py
+++ src/main.py
@@ -3,24 +3,25 @@ import os, shutil
 #Recursive function that copies the content of a directory to another directory, including all files and subdirectories.
 #Delete the contents of public directory prior to copying. 
 
-def copy_process(static, dest):
-    for object in os.listdir(static):
-        static_path = os.path.join(static, object)
+def copy_process(src, dest):
+    for object in os.listdir(src):
+        src_path = os.path.join(src, object)
         dest_path = os.path.join(dest, object)
-        if os.path.isdir(static_path):
+        if os.path.isdir(src_path):
             os.makedirs(dest_path)
-            copy_process(static_path, dest_path)
+            copy_process(src_path, dest_path)
         else:
-            shutil.copyfile(static_path, dest_path)
+            shutil.copyfile(src_path, dest_path)
 
-def copy_directory(static, dest):
+def copy_directory(src, dest):
     if os.path.exists(dest):
         shutil.rmtree(dest)
-        copy_process(static, dest)
+        copy_process(src, dest)
     else:
         raise ValueError('Destination directory does not exist')
 
 def main():
+    copy_directory('src/static', 'public')
     
 
 main()
